64a4b985a7793f4b66fa80c290eb113712db8e05
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-05-04 16:55:52 +0200

    added DAPs in list

--- p_parameters/07_itemsets.R
+++ p_parameters/07_itemsets.R
@@ -16,39 +16,34 @@ for (i in 1:length(files)) {
   if (str_detect(files[i],"^PROCEDURES")) {
     
 #--------------------------
-
 ####### Codes for tests for gestational diabetes ###############
-#item_set_codes_our_study[["gestational_diabetes"]][["ICD9"]] <- c("648.8") #ICD-9-MC
-#item_set_codes_our_study[["gestational_diabetes"]][["ICD10"]] <- c("O24.4") #ICD-10 Spanish version
-item_set_codes_our_study[["gestational_diabetes"]][["ITA_procedures_coding_system"]] <- c("90.26.7") #glucose tolerance test 60-120 minutes
+item_set_codes_our_study[["gestational_diabetes"]][["ARS"]][["ITA_procedures_coding_system"]] <- c("90.26.7") #glucose tolerance test 60-120 minutes
+#item_set_codes_our_study[["gestational_diabetes"]][["FISABIO"]][["ICD9"]] <- c("648.8") #ICD-9-MC
+#item_set_codes_our_study[["gestational_diabetes"]][["FISABIO"]][["ICD10"]] <- c("O24.4") #ICD-10 Spanish version
 
 
 ####### Codes for fetal nuchal translucency ###############
-#item_set_codes_our_study[["fetal_nuchal_translucency"]][[""]] <- c("MA2JE") # maybe also MA1AE 
+#item_set_codes_our_study[["fetal_nuchal_translucency"]][["THL"]][[""]] <- c("MA2JE") # maybe also MA1AE 
 
 
 ####### Codes for amniocentesis ###############
-item_set_codes_our_study[["amniocentesis"]][["ITA_procedures_coding_system"]] <- c("75.10.2", "75.10.3")
-#item_set_codes_our_study[["amniocentesis"]][[""]] <- c("88.78") # ITA_procedures_coding_system
-#item_set_codes_our_study[["amniocentesis"]][[""]] <- c("MAA00") 
+item_set_codes_our_study[["amniocentesis"]][["ARS"]][["ITA_procedures_coding_system"]] <- c("75.10.2", "75.10.3")
+#item_set_codes_our_study[["amniocentesis"]][["UNIME"]][["ITA_procedures_coding_system"]] <- c("88.78")
+#item_set_codes_our_study[["amniocentesis"]][["THL"]][[""]] <- c("MAA00")
+
 
 
 ####### Codes for Chorionic Villus Sampling ###############
- item_set_codes_our_study[["Chorionic_Villus_Sampling"]][["ITA_procedures_coding_system"]] <- c("75.10.1")
-# item_set_codes_our_study[["Chorionic_Villus_Sampling"]][[""]] <- c("MAA10")
+ item_set_codes_our_study[["Chorionic_Villus_Sampling"]][["ARS"]][["ITA_procedures_coding_system"]] <- c("75.10.1")
+# item_set_codes_our_study[["Chorionic_Villus_Sampling"]][["THL"]][[""]] <- c("MAA10")
  
 
 # ####### Codes for tests for others ###############
-item_set_codes_our_study[["others"]][["ITA_procedures_coding_system"]] <- c("75.34.1")
-# item_set_codes_our_study[["others"]][["ICD9"]] <- c("72.", "73.", "74.", "75.",  #ICD-9-MC, Chapter 13
-#                                                     "645.1", "644.2", "650",     #ICD9-CM, as diagnosi
-#                                                     "72.", "73.", "74.", "75.",  #ICD9-CM, as procedures
-#                                                     "640.xx", "642.xx", "648.xx", "651.xx", "652.xx", "653.xx", "654.xx", "655.xx", "656.xx", "657.xx", "658.xx", "659.xx") #as diagnosis
+item_set_codes_our_study[["others"]][["ARS"]][["ITA_procedures_coding_system"]] <- c("75.34.1")
+#item_set_codes_our_study[["others"]][["UNIME"]][["ICD9"]] <- c("645.1", "644.2", "650", "72.", "73.", "74.", "75.", "640.xx", "642.xx", "648.xx", "651.xx", "652.xx", "653.xx", "654.xx", "655.xx", "656.xx", "657.xx", "658.xx", "659.xx") 
+#item_set_codes_our_study[["others"]][["FISABIO"]][["ICD9"]] <- c("72.", "73.", "74.", "75.")
+#item_set_codes_our_study[["others"]][["FISABIO"]][["ICD10"]] <- c("102", "109", "10A", "10D", "10E", "10H", "10J", "10P", "10Q", "10S", "10T", "10Y") #ICD-10-Spanish version, Chapter 1 Obstetricia
 
- 
-# item_set_codes_our_study[["others"]][["ICD10"]] <- c("102", "109", "10A", "10D", "10E", "10H", "10J", "10P", "10Q", "10S", "10T", "10Y") #ICD-10-Spanish version, Chapter 1 Obstetricia
-# item_set_codes_our_study[["others"]][[""]] <- c()
-# 
   }
 }
 
