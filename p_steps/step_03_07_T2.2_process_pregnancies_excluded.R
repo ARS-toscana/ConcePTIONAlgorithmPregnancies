a6b15eea6a7288632ddd0bddc8d70c00e94a3ceb
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-05-26 15:38:15 +0200

    minor change

--- p_steps/step_03_07_T2.2_process_pregnancies_excluded.R
+++ p_steps/step_03_07_T2.2_process_pregnancies_excluded.R
@@ -22,6 +22,15 @@ if(dim(D3_excluded_pregnancies_from_EUROCAT)[1]==0) {
   D3_excluded_pregnancies_from_EUROCAT<-data.table(EUROCAT=character(0))
 }
 
+D3_excluded_pregnancies_from_ITEMSETS<-data.table()
+for (i in 1:length(files)) {
+  if (str_detect(files[i],"^D3_excluded_pregnancies_from_ITEMSETS")) { 
+    load(paste0(dirtemp,files[i],".RData")) 
+  }
+} 
+if(dim(D3_excluded_pregnancies_from_ITEMSETS)[1]==0) {
+  D3_excluded_pregnancies_from_ITEMSETS<-data.table(ITEMSETS=character(0))
+}
 
 #load(paste0(dirtemp,"D3_excluded_pregnancies_from_PROMPT.RData"))
 load(paste0(dirtemp,"D3_excluded_pregnancies_from_CONCEPTSETS.RData"))
@@ -29,13 +38,13 @@ load(paste0(dirtemp,"D3_excluded_pregnancies_from_CONCEPTSETS.RData"))
 #load(paste0(dirtemp,"D3_excluded_pregnancies_from_ITEMSETS.RData"))
 
 # put together all the D3_Stream..
-groups_of_excluded_pregnancies<-rbind(D3_excluded_pregnancies_from_CONCEPTSETS,D3_excluded_pregnancies_from_EUROCAT,D3_excluded_pregnancies_from_PROMPT, fill=T)[is.na(pregnancy_with_dates_out_of_range),pregnancy_with_dates_out_of_range:=0][is.na(no_linked_to_person),no_linked_to_person:=0][is.na(person_not_female),person_not_female:=0][is.na(person_not_in_fertile_age),person_not_in_fertile_age:=0][is.na(pregnancy_start_in_spells),pregnancy_start_in_spells:=0][is.na(pregnancy_end_in_spells),pregnancy_end_in_spells:=0]
-groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[,.(pregnancy_id,person_id,survey_id,visit_occurrence_id,PROMPT,EUROCAT,CONCEPTSETS,CONCEPTSET, pregnancy_with_dates_out_of_range,no_linked_to_person,person_not_female,person_not_in_fertile_age,pregnancy_start_in_spells,pregnancy_end_in_spells)]# ITEMSETS
+groups_of_excluded_pregnancies<-rbind(D3_excluded_pregnancies_from_CONCEPTSETS,D3_excluded_pregnancies_from_EUROCAT,D3_excluded_pregnancies_from_PROMPT,D3_excluded_pregnancies_from_ITEMSETS, fill=T)[is.na(pregnancy_with_dates_out_of_range),pregnancy_with_dates_out_of_range:=0][is.na(no_linked_to_person),no_linked_to_person:=0][is.na(person_not_female),person_not_female:=0][is.na(person_not_in_fertile_age),person_not_in_fertile_age:=0][is.na(pregnancy_start_in_spells),pregnancy_start_in_spells:=0][is.na(pregnancy_end_in_spells),pregnancy_end_in_spells:=0]
+groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[,.(pregnancy_id,person_id,survey_id,visit_occurrence_id,PROMPT,EUROCAT,CONCEPTSETS,CONCEPTSET,ITEMSETS, pregnancy_with_dates_out_of_range,no_linked_to_person,person_not_female,person_not_in_fertile_age,pregnancy_start_in_spells,pregnancy_end_in_spells)]# 
 
 groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[is.na(PROMPT),PROMPT:="no"]
 groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[is.na(EUROCAT),EUROCAT:="no"]
 groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[is.na(CONCEPTSETS),CONCEPTSETS:="no"]
-#groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[is.na(ITEMSETS),ITEMSETS:="no"]
+groups_of_excluded_pregnancies<-groups_of_excluded_pregnancies[is.na(ITEMSETS),ITEMSETS:="no"]
 
 #create var reason_for_exclusion
 D3_excluded_pregnancies<-groups_of_excluded_pregnancies[pregnancy_with_dates_out_of_range==1, reason_for_exclusion:="pregnancy_with_dates_out_of_range"]
