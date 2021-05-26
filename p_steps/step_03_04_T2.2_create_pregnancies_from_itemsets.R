9e928a86e215aba35f644f6c7a07bea5ad1e74d2
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-05-26 13:23:51 +0200

    updated step 03_04

--- p_steps/step_03_04_T2.2_create_pregnancies_from_itemsets.R
+++ p_steps/step_03_04_T2.2_create_pregnancies_from_itemsets.R
@@ -23,14 +23,29 @@ if (this_datasource_has_itemsets_stream){
   
   
   
-  
   # order dataset for person_id, 
   setorderv(dataset_item_sets,c("person_id","date"), na.last = T)
   
-  # start creating pregnancy_end_date, pregnancy_start_date, pregnancy_ongoing_date
-  dataset_item_sets<-dataset_item_sets[item_set%chin%dataset_item_sets, pregnancy_start_date:=date]
-  dataset_item_sets<-dataset_item_sets[item_set%chin%dataset_item_sets, pregnancy_ongoing_date:=date]
-  dataset_item_sets<-dataset_item_sets[item_set%chin%dataset_item_sets, pregnancy_end_date:=date]
+  
+  if (thisdatasource=="TEST" | thisdatasource=="BIFAP"){
+    
+    # transform data var
+    dataset_item_sets<-dataset_item_sets[,mo_source_value:=as.character(mo_source_value)]
+    dataset_item_sets<-dataset_item_sets[item_set=="LastMestrualPeriod",mo_source_value:=(mo_source_value)]
+    
+    # start creating pregnancy_ongoing_date
+    dataset_item_sets<-dataset_item_sets[,`:=`(pregnancy_ongoing_date=date, meaning_ongoing_date=paste0("from_itemset_",item_set))]
+    
+    # then pregnancy_start_date
+    dataset_item_sets<-dataset_item_sets[item_set=="GestationalAge" & mo_unit=="days", `:=`(pregnancy_start_date=pregnancy_ongoing_date-mo_source_value, meaning_start_date=paste0("from_itemset_",item_set)) ]
+    dataset_item_sets<-dataset_item_sets[item_set=="GestationalAge" & mo_unit=="weeks", `:=`(pregnancy_start_date=pregnancy_ongoing_date-(mo_source_value*7), meaning_start_date=paste0("from_itemset_",item_set)) ]
+    
+    dataset_item_sets<-dataset_item_sets[item_set=="LastMestrualPeriod", `:=`(pregnancy_start_date=ymd(mo_source_value), meaning_start_date=paste0("from_itemset_",item_set)) ]
+    
+    
+  }
+  
+  
   
   dataset_item_sets<-dataset_item_sets[!is.na(pregnancy_start_date), meaning_start_date:=paste0("from_conceptset_",concept_set)]
   dataset_item_sets<-dataset_item_sets[!is.na(pregnancy_ongoing_date), meaning_ongoing_date:=paste0("from_conceptset_",concept_set)]
