ae5478e03e7ee2c4181bbcf9c36fd6469f5d7739
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-06-03 12:34:33 +0200

    added condmeaning

--- p_parameters/06_algorithms.R
+++ p_parameters/06_algorithms.R
@@ -214,3 +214,26 @@ dictonary_of_itemset_this_datasource<-vector(mode="list")
 for (i in 1:length(dictonary_of_itemset$TYPE)) {
   if(names(dictonary_of_itemset$TYPE)[[i]]==thisdatasource) dictonary_of_itemset_this_datasource<-dictonary_of_itemset$TYPE[[i]]
 }
+
+
+
+
+
+
+# we need to create two groups of meanings: one referring to hospitals HOSP (excluding emergency care) and one referring to primary care PC
+
+meanings_of_this_study<-vector(mode="list")
+meanings_of_this_study[["HOSP"]]=c("hospitalisation_primary","hospitalisation_secondary","hospital_diagnosis","hopitalisation_diagnosis_unspecified","episode_primary_diagnosis","episode_secondary_diagnosis","diagnosis_procedure","hospitalisation_associated","hospitalisation_linked","HH","NH")
+meanings_of_this_study[["PC"]]=c("primary_care_event","primary_care_diagnosis","primary_care_events_BIFAP","primary_care_antecedents_BIFAP","primary_care_condicionants_BIFAP")
+
+# create two conditions on the meaning_of_event variable, associated to HOSP and to PC as listed above
+
+condmeaning <- list()
+for (level1 in c("HOSP","PC")) {
+  for (meaning in meanings_of_this_study[[level1]]) {
+    if (length(condmeaning[[level1]])==0) {condmeaning[[level1]]=paste0("meaning_of_event=='",meanings_of_this_study[[level1]][[1]],"'")
+    }else{
+      condmeaning[[level1]]=paste0(condmeaning[[level1]], " | meaning_of_event=='",meaning,"'")
+    }
+  }
+}
