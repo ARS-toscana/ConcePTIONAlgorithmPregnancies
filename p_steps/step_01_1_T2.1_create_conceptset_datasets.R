b0adf284ef4826e14a86772bbd988c637e6e74ce
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-06-16 14:52:55 +0200

    added concept sets for pregancy outcomes

--- p_steps/step_01_1_T2.1_create_conceptset_datasets.R
+++ p_steps/step_01_1_T2.1_create_conceptset_datasets.R
@@ -57,3 +57,30 @@ CreateConceptSetDatasets(concept_set_names = c(concept_sets_of_our_study_procedu
                          vocabularies_with_keep_dot = c("ITA_procedures_coding_system")#,
                          #vocabularies_with_dot_wildcard=c("READ")
 )
+
+
+## concept sets per PREGNANCY outcomes
+
+print('RETRIEVE FROM CDM RECORDS CORRESPONDING TO CONCEPT SETS FOR  PREGNANCY OUTCOMES')
+
+
+CreateConceptSetDatasets(concept_set_names = c(concept_set_our_study_pre),
+                         dataset = ConcePTION_CDM_tables,
+                         codvar = ConcePTION_CDM_codvar,
+                         datevar= ConcePTION_CDM_datevar,
+                         EAVtables=ConcePTION_CDM_EAV_tables,
+                         EAVattributes= ConcePTION_CDM_EAV_attributes_this_datasource,
+                         dateformat= "YYYYmmdd",
+                         vocabulary = ConcePTION_CDM_coding_system_cols,
+                         rename_col = list(person_id=person_id,date=date),
+                         concept_set_domains = concept_set_domains,
+                         concept_set_codes =	concept_set_codes_our_study_pre,
+                         concept_set_codes_excl = concept_set_codes_our_study_pre_excl,
+                         discard_from_environment = F,
+                         dirinput = dirinput,
+                         diroutput = dirtemp,
+                         extension = c("csv"),
+                         vocabularies_with_keep_dot = c("ITA_procedures_coding_system")#,
+                         #vocabularies_with_dot_wildcard=c("READ")
+)
+
