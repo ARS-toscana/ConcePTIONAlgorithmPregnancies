aec1b6b618512385e21ed8ba7cac0eebe4373918
RosaMGini <rosa.gini@ars.toscana.it>
2021-06-23 17:42:22 +0200

    fixed control for supported coding systems

--- p_parameters/08_check_coding_system.R
+++ p_parameters/08_check_coding_system.R
@@ -1,12 +1,12 @@
 ## check coding system
 
 allcodes_inMETADATA<- unique(unlist(str_split(unique(METADATA[type_of_metadata=="list_of_values" & (columnname=="event_record_vocabulary" | columnname=="	
-procedure_code_vocabulary" | columnname=="mo_record_vocabulary" | columnname=="so_unit"),values])," ")))
+procedure_code_vocabulary" | columnname=="mo_record_vocabulary" | columnname=="so_unit"),values])," "))) # discard from this list any string that contains 'free_text' as a substring
                    
 concept_set_prova<-names(concept_set_domains=="Diagnosis")[1]
 coding_system_in_concept_sets<-names(concept_set_codes_our_study[[concept_set_prova]])
 
 if(sum(coding_system_in_concept_sets %notin% allcodes_inMETADATA)*1 >0) {
-  print("There is an error in coding system: one or more coding system in concept set is/are not present in METADATA. Please check your data!")
+  print("There is an error in coding system: one or more coding system in your data are not included in the list of coding systems supported by this script. Please contact the script programmer.") # display the coding systems that are missing
   
 }
