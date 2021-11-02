6f7e4b4a8233d1cb0d3f870ecb24f0847b99f773
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-11-02 12:29:59 +0100

    added condition for br_prompts

--- ConcePTIONAlgorithmPregnancies-version_2.0/p_steps/step_01_4_T2.1_create_prompt_datasets.R
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_steps/step_01_4_T2.1_create_prompt_datasets.R
@@ -4,38 +4,40 @@
 # RETRIEVE FROM SURVEY_ID ALL RECORDS WHOSE meaning IS "birth_registry_mother" AND SAVE
 
 # TO DO: collect and rbind from all files whose name starts with 'SURVEY_ID'
-
-SURVEY_ID_BR <- data.table()
-files<-sub('\\.csv$', '', list.files(dirinput))
-for (i in 1:length(files)) {
-  if (str_detect(files[i],"^SURVEY_ID")) {
-    
-    SURVEY_ID_BR <-rbind(SURVEY_ID_BR,fread(paste0(dirinput,files[i],".csv"), colClasses = list(character="person_id"))[survey_meaning %in% unlist(meaning_of_survey_our_study_this_datasource),])
-
+if (this_datasource_has_br_prompt) {
+  
+  SURVEY_ID_BR <- data.table()
+  files<-sub('\\.csv$', '', list.files(dirinput))
+  for (i in 1:length(files)) {
+    if (str_detect(files[i],"^SURVEY_ID")) {
+      
+      SURVEY_ID_BR <-rbind(SURVEY_ID_BR,fread(paste0(dirinput,files[i],".csv"), colClasses = list(character="person_id"))[survey_meaning %in% unlist(meaning_of_survey_our_study_this_datasource),])
+  
+    }
   }
+
+
+
+  ##### Description #####
+  if(nrow(SURVEY_ID_BR)!=0){
+    DescribeThisDataset(Dataset = SURVEY_ID_BR,
+                        Individual=T,
+                        ColumnN=NULL,
+                        HeadOfDataset=FALSE,
+                        StructureOfDataset=FALSE,
+                        NameOutputFile="SURVEY_ID_BR",
+                        Cols=list("survey_origin", "survey_meaning"),
+                        ColsFormat=list("categorical", "categorical"),
+                        DateFormat_ymd=FALSE,
+                        DetailInformation=TRUE,
+                        PathOutputFolder= dirdescribe01_prompts)
+  }
+  ##### End Description #####
+  
+  
+  save(SURVEY_ID_BR, file=paste0(dirtemp,"SURVEY_ID_BR.RData"))
+  rm(SURVEY_ID_BR)
 }
-
-
-
-##### Description #####
-if(nrow(SURVEY_ID_BR)!=0){
-  DescribeThisDataset(Dataset = SURVEY_ID_BR,
-                      Individual=T,
-                      ColumnN=NULL,
-                      HeadOfDataset=FALSE,
-                      StructureOfDataset=FALSE,
-                      NameOutputFile="SURVEY_ID_BR",
-                      Cols=list("survey_origin", "survey_meaning"),
-                      ColsFormat=list("categorical", "categorical"),
-                      DateFormat_ymd=FALSE,
-                      DetailInformation=TRUE,
-                      PathOutputFolder= dirdescribe01_prompts)
-}
-##### End Description #####
-
-
-save(SURVEY_ID_BR, file=paste0(dirtemp,"SURVEY_ID_BR.RData"))
-
 
 # RETRIEVE FROM VISIT_OCCURRENCE_ID ALL RECORDS WHOSE meaning IS "first_encounter_for_ongoing_pregnancy", "service_before_termination" , "service_for_ongoing_pregnancy" AND SAVE
 
@@ -71,4 +73,4 @@ if (this_datasource_has_visit_occurrence_prompt) {
   
 }
 
-rm(SURVEY_ID_BR)
+
