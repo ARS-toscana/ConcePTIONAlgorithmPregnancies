c4cedb22e07987ca1cf42ebe8f88d0358022f087
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-11-02 14:30:01 +0100

    adapted 04_prompts

--- ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters/04_prompts.R
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters/04_prompts.R
@@ -1,5 +1,14 @@
-################################ MEANING_OF_SURVEY ##################################
+###################################################################
+###################  MEANING_OF_SURVEY   ##########################
+###################################################################
+
 meaning_of_survey_our_study <- vector(mode="list")
+
+meaning_of_survey_our_study[["TO_ADD"]][["livebirth_or_stillbirth"]]<-list("birth_registry_mother","algorithm_pregnancy") 
+meaning_of_survey_our_study[["TO_ADD"]][["ongoing_pregnancy"]]<-list("encounter_registry")
+meaning_of_survey_our_study[["TO_ADD"]][["spontaneous_abortion"]]<-list("spontaneous_abortion_registry","algorithm_pregnancy") 
+meaning_of_survey_our_study[["TO_ADD"]][["induced_termination"]]<-list("induced_termination_registry","algorithm_pregnancy") 
+meaning_of_survey_our_study[["TO_ADD"]][["other"]]<-list()
 
 meaning_of_survey_our_study[["ARS"]][["livebirth_or_stillbirth"]]<-list("birth_registry_mother")
 meaning_of_survey_our_study[["ARS"]][["ongoing_pregnancy"]]<-list()
@@ -7,84 +16,6 @@ meaning_of_survey_our_study[["ARS"]][["spontaneous_abortion"]]<-list("spontaneou
 meaning_of_survey_our_study[["ARS"]][["induced_termination"]]<-list("induced_termination_registry")
 meaning_of_survey_our_study[["ARS"]][["other"]]<-list()
 
-meaning_of_survey_our_study[["UOSL"]][["livebirth_or_stillbirth"]]<-list("birth_registry_mother") #, "birth_registry_father", "birth_registry_child"
-meaning_of_survey_our_study[["UOSL"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["UOSL"]][["spontaneous_abortion"]]<-list()
-meaning_of_survey_our_study[["UOSL"]][["induced_termination"]]<-list()
-meaning_of_survey_our_study[["UOSL"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["AARHUS"]][["livebirth_or_stillbirth"]]<-list("birth_registry")
-meaning_of_survey_our_study[["AARHUS"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["AARHUS"]][["spontaneous_abortion"]]<-list()
-meaning_of_survey_our_study[["AARHUS"]][["induced_termination"]]<-list()
-meaning_of_survey_our_study[["AARHUS"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["CHUT"]][["livebirth_or_stillbirth"]]<-list("pregnancy_characteristics")
-meaning_of_survey_our_study[["CHUT"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["CHUT"]][["spontaneous_abortion"]]<-list("pregnancy_characteristics")
-meaning_of_survey_our_study[["CHUT"]][["induced_termination"]]<-list("pregnancy_characteristics")
-meaning_of_survey_our_study[["CHUT"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["GePaRD"]][["livebirth_or_stillbirth"]]<-list("algorithm_pregnancy") 
-meaning_of_survey_our_study[["GePaRD"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["GePaRD"]][["spontaneous_abortion"]]<-list("algorithm_pregnancy") 
-meaning_of_survey_our_study[["GePaRD"]][["induced_termination"]]<-list("algorithm_pregnancy") 
-meaning_of_survey_our_study[["GePaRD"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["BIFAP"]][["livebirth_or_stillbirth"]]<-list("algorithm_pregnancy")
-meaning_of_survey_our_study[["BIFAP"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["BIFAP"]][["spontaneous_abortion"]]<-list("algorithm_pregnancy")
-meaning_of_survey_our_study[["BIFAP"]][["induced_termination"]]<-list("algorithm_pregnancy")
-meaning_of_survey_our_study[["BIFAP"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["FISABIO"]][["livebirth_or_stillbirth"]]<-list("META-B_mother", "RMPCV_mother")
-meaning_of_survey_our_study[["FISABIO"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["FISABIO"]][["spontaneous_abortion"]]<-list()
-meaning_of_survey_our_study[["FISABIO"]][["induced_termination"]]<-list()
-meaning_of_survey_our_study[["FISABIO"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["SIDIAP"]][["livebirth_or_stillbirth"]]<-list("birth_registry")
-meaning_of_survey_our_study[["SIDIAP"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["SIDIAP"]][["spontaneous_abortion"]]<-list("spontaneous_abortion_registry")
-meaning_of_survey_our_study[["SIDIAP"]][["induced_termination"]]<-list("induced_termination_registry")
-meaning_of_survey_our_study[["SIDIAP"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["UNIME"]][["livebirth_or_stillbirth"]]<-list("birth_registry_mother")
-meaning_of_survey_our_study[["UNIME"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["UNIME"]][["spontaneous_abortion"]]<-list("spontaneous_abortion_registry")
-meaning_of_survey_our_study[["UNIME"]][["induced_termination"]]<-list("induced_termination_registry")
-meaning_of_survey_our_study[["UNIME"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["THL"]][["livebirth_or_stillbirth"]]<-list("birth_registry")
-meaning_of_survey_our_study[["THL"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["THL"]][["spontaneous_abortion"]]<-list()
-meaning_of_survey_our_study[["THL"]][["induced_termination"]]<-list("induced_termination_registry")
-meaning_of_survey_our_study[["THL"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["CPRD"]][["livebirth_or_stillbirth"]]<-list("pregnancy_register")
-meaning_of_survey_our_study[["CPRD"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["CPRD"]][["spontaneous_abortion"]]<-list("pregnancy_register")
-meaning_of_survey_our_study[["CPRD"]][["induced_termination"]]<-list("pregnancy_register")
-meaning_of_survey_our_study[["CPRD"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["CNR-IFC"]][["livebirth_or_stillbirth"]]<-list("birth_registry_mother", "birth_registry_child")
-meaning_of_survey_our_study[["CNR-IFC"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["CNR-IFC"]][["spontaneous_abortion"]]<-list()
-meaning_of_survey_our_study[["CNR-IFC"]][["induced_termination"]]<-list()
-meaning_of_survey_our_study[["CNR-IFC"]][["other"]]<-list()
-
-meaning_of_survey_our_study[["PEDIANET"]][["livebirth_or_stillbirth"]]<-list()
-meaning_of_survey_our_study[["PEDIANET"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["PEDIANET"]][["spontaneous_abortion"]]<-list()
-meaning_of_survey_our_study[["PEDIANET"]][["induced_termination"]]<-list()
-meaning_of_survey_our_study[["PEDIANET"]][["other"]]<-list()
-
-
-meaning_of_survey_our_study[["TEST"]][["livebirth_or_stillbirth"]]<-list("algorithm_pregnancy") 
-meaning_of_survey_our_study[["TEST"]][["ongoing_pregnancy"]]<-list()
-meaning_of_survey_our_study[["TEST"]][["spontaneous_abortion"]]<-list("algorithm_pregnancy") 
-meaning_of_survey_our_study[["TEST"]][["induced_termination"]]<-list("algorithm_pregnancy") 
-meaning_of_survey_our_study[["TEST"]][["other"]]<-list()
 
 meaning_of_survey_our_study_this_datasource<-vector(mode="list")
 
@@ -94,13 +25,14 @@ for (i in 1:length(meaning_of_survey_our_study)) {
 
 
 
-################################################################################
-################################################################################
-################################################################################
 
+###################################################################
+###################  MEANING_OF_VISIT   ##########################
+###################################################################
 
 meaning_of_visit_our_study <- vector(mode="list")
-meaning_of_visit_our_study[["ARS"]]<-list("first_encounter_for_ongoing_pregnancy", "service_before_termination" , "service_for_ongoing_pregnancy")
+meaning_of_visit_our_study[["TO_ADD"]]<-list("service_before_termination")
+meaning_of_visit_our_study[["ARS"]]<-list("first_encounter_for_ongoing_pregnancy", "service_before_termination","service_for_ongoing_pregnancy")
 
 
 meaning_of_visit_our_study_this_datasource<-vector(mode="list")
