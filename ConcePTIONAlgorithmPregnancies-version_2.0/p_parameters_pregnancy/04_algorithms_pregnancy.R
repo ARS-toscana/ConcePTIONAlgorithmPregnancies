ac5eb099c958b9a05337ad75c34c678a3e55ac90
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-11-03 16:33:56 +0100

    adapted to str

---
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/04_algorithms_pregnancy.R
@@ -0,0 +1,63 @@
+## ALGORITMH FOR PREGNANCY SCRIPT
+
+#datasource that do not modify record from PROMPT
+datasource_that_does_not_modify_PROMPT <- c("TO_ADD","UOSL") #@ use "TO_ADD" as example
+this_datasource_does_not_modify_PROMPT <- ifelse(thisdatasource %in% datasource_that_does_not_modify_PROMPT,TRUE,FALSE) 
+
+
+## FIXING FOR CODING SYSTEMS
+
+# fix for ICPC2P
+for (conceptset in concept_set_our_study){
+  if (length(concept_set_codes_our_study[[conceptset]][["ICPC2P"]]) >0 ){
+    concept_set_codes_our_study[[conceptset]][["ICPC"]] <- unique(c(concept_set_codes_our_study[[conceptset]][["ICPC"]],substr(concept_set_codes_our_study[[conceptset]][["ICPC2P"]],1,3)))
+  }
+}
+
+
+
+# fix for ICD10GM
+
+for (conceptset in concept_set_our_study){
+  #print(conceptset)
+  if (concept_set_domains[[conceptset]] == "Diagnosis"){
+    concept_set_codes_our_study[[conceptset]][["ICD10GM"]] <- concept_set_codes_our_study[[conceptset]][["ICD10"]]
+  }
+}
+
+
+# fix for ICD10CM
+for (conceptset in concept_set_our_study){
+  if (concept_set_domains[[conceptset]] == "Diagnosis"){
+    concept_set_codes_our_study[[conceptset]][["ICD10CM"]] <- concept_set_codes_our_study[[conceptset]][["ICD10"]]
+  }
+}
+
+# fix for CIM10
+for (conceptset in concept_set_our_study){
+  if (concept_set_domains[[conceptset]] == "Diagnosis"){
+    concept_set_codes_our_study[[conceptset]][["CIM10"]] <- concept_set_codes_our_study[[conceptset]][["ICD10"]]
+  }
+}
+
+
+# fix for SNOMED3
+for (conceptset in concept_set_our_study){
+  if (concept_set_domains[[conceptset]] == "Diagnosis"){
+    concept_set_codes_our_study[[conceptset]][["SNOMED3"]] <- concept_set_codes_our_study[[conceptset]][["SNOMED"]]
+  }
+}
+
+save(concept_set_codes_our_study,file=paste0(direxp,"concept_set_codes_our_study.RData"))
+save(concept_set_codes_our_study_excl,file=paste0(direxp,"concept_set_codes_our_study_excl.RData"))
+save(concept_set_codes_our_study,file=paste0(dirsmallcountsremoved,"concept_set_codes_our_study.RData"))
+save(concept_set_codes_our_study_excl,file=paste0(dirsmallcountsremoved,"concept_set_codes_our_study_excl.RData"))
+
+if (this_datasource_has_subpopulations == TRUE){
+  for (subpop in subpopulations[[thisdatasource]]){
+    save(concept_set_codes_our_study,file=paste0(direxpsubpop[[subpop]],"concept_set_codes_our_study.RData"))
+    save(concept_set_codes_our_study_excl,file=paste0(direxpsubpop[[subpop]],"concept_set_codes_our_study_excl.RData"))
+    save(concept_set_codes_our_study_excl,file=paste0(dirsmallcountsremovedsubpop[[subpop]],"concept_set_codes_our_study_excl.RData"))
+    
+  }
+}
--- ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/07_algorithms_pregnancy.R
+++
@@ -1,3 +0,0 @@
-#datasource that do not modify record from PROMPT
-datasource_that_does_not_modify_PROMPT <- c("TO_ADD","UOSL") #@ use "TO_ADD" as example
-this_datasource_does_not_modify_PROMPT <- ifelse(thisdatasource %in% datasource_that_does_not_modify_PROMPT,TRUE,FALSE) 
