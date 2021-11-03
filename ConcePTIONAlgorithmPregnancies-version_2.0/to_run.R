b3882868b110ea121241686aa01a33d6b18babd6
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-11-03 11:19:22 +0100

    cleaned up steps

--- ConcePTIONAlgorithmPregnancies-version_2.0/to_run.R
+++ ConcePTIONAlgorithmPregnancies-version_2.0/to_run.R
@@ -40,15 +40,15 @@ system.time(source(paste0(thisdir,"/p_steps/step_01_5_T2.1_create_itemsets_datas
 #system.time(source(paste0(thisdir,"/p_steps/step_02_T2.2_count_codes.R")))
 
 # 03 CREATE PREGNANCIES 
-source(paste0(thisdir,"/p_steps/step_03_01_T2.2_create_pregnancies_from_prompts.R")) #--> D3_Stream_PROMPTS
-source(paste0(thisdir,"/p_steps/step_03_02_T2.2_create_pregnancies_from_conceptsets.R")) #--> D3_Stream_CONCEPTSETS
-source(paste0(thisdir,"/p_steps/step_03_03_T2.2_create_pregnancies_from_EUROCAT.R")) #--> D3_Stream_EUROCAT
-source(paste0(thisdir,"/p_steps/step_03_04_T2.2_create_pregnancies_from_itemsets.R")) #--> D3_Stream_ITEMSETS 
+source(paste0(thisdir,"/p_steps/step_03_01_T2.2_create_pregnancies_from_prompts.R")) 
+source(paste0(thisdir,"/p_steps/step_03_02_T2.2_create_pregnancies_from_conceptsets.R")) 
+source(paste0(thisdir,"/p_steps/step_03_03_T2.2_create_pregnancies_from_EUROCAT.R")) 
+source(paste0(thisdir,"/p_steps/step_03_04_T2.2_create_pregnancies_from_itemsets.R")) 
 
 source(paste0(thisdir,"/p_steps/step_03_05a_T2.2_internal_consistency_for_prompts.R")) 
 source(paste0(thisdir,"/p_steps/step_03_05b_T2.2_internal_consistency_for_conceptsets.R")) 
 source(paste0(thisdir,"/p_steps/step_03_05c_T2.2_internal_consistency_for_EUROCAT.R")) 
-source(paste0(thisdir,"/p_steps/step_03_05d_T2.2_internal_consistency_for_itemsets.R")) #--> aggiungere PERSONS con id di BIFAP finti!
+source(paste0(thisdir,"/p_steps/step_03_05d_T2.2_internal_consistency_for_itemsets.R")) 
 
 source(paste0(thisdir,"/p_steps/step_03_06_1_T2.2_process_pregnancies_excluded.R")) 
 source(paste0(thisdir,"/p_steps/step_03_06_2_T2.3_merge_stream_of_same_person.R"))
