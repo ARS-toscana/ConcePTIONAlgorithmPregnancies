3ec94f6868573528df82af0b03600fbdcd04c71e
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-11-17 10:55:42 +0100

    added itemsets from MO UOSL

--- ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/02_itemsets/02_itemsets_UOSL.R
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/02_itemsets/02_itemsets_UOSL.R
@@ -63,3 +63,32 @@ dictonary_of_itemset_pregnancy[["TYPE"]][["UOSL"]][["T"]]<-list(list("MBRN", "10
 dictonary_of_itemset_pregnancy[["TYPE"]][["UOSL"]][["MD"]]<-list()
 dictonary_of_itemset_pregnancy[["TYPE"]][["UOSL"]][["ECT"]]<-list()
 dictonary_of_itemset_pregnancy[["TYPE"]][["UOSL"]][["UNK"]]<-list(list("MBRN", "7"), list("MBRN", "8"), list("MBRN", "9"))
+
+
+
+
+
+##### FROM MEDICAL_OBSERVATION
+
+### specification LastMestrualPeriod
+itemsetMED_AVpair_pregnancy[["LastMestrualPeriod"]][[files[i]]][["UOSL"]] <- list(list("MBRN","smensd_dato"))
+
+
+### specification GestationalAge
+itemsetMED_AVpair_pregnancy[["GestationalAge"]][[files[i]]][["UOSL"]] <- list()
+
+
+### specification PregnancyTest
+itemsetMED_AVpair_pregnancy[["PregnancyTest"]][[files[i]]][["UOSL"]] <- list()
+
+
+
+################################ DICTINARY OF PregnancyTest ##################################
+
+dictonary_of_itemset_PregnancyTest[["PregnancyTest"]][["positive"]]<-list(list("positive")) 
+
+
+
+################################ PARAMETERS for PregnancyTest ##################################
+days_from_start_PregnancyTest <- 30
+days_to_end_PregnancyTest <- 280
