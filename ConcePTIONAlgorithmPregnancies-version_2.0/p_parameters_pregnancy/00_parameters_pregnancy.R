f3de9f30d24d9a332febe169ab3e2fb47198b281
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-11-17 10:56:56 +0100

    Updated UOSL parameters (MO)

--- ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/00_parameters_pregnancy.R
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/00_parameters_pregnancy.R
@@ -5,7 +5,7 @@ datasources_EUROCAT <- c("TO_ADD") #@ use "TO_ADD" as example
 thisdatasource_has_EUROCAT <- ifelse(thisdatasource %in% datasources_EUROCAT,TRUE,FALSE)
 
 #datasource with itemsets stream from medical observation
-datasource_with_itemsets_stream_from_medical_obs <- c("TO_ADD","GePaRD","BIFAP","PHARMO") #@ use "TO_ADD" as example
+datasource_with_itemsets_stream_from_medical_obs <- c("TO_ADD","GePaRD","BIFAP","PHARMO","UOSL") #@ use "TO_ADD" as example
 this_datasource_has_itemsets_stream_from_medical_obs <- ifelse(thisdatasource %in% datasource_with_itemsets_stream_from_medical_obs,TRUE,FALSE) 
 
 #datasource with  prompt
