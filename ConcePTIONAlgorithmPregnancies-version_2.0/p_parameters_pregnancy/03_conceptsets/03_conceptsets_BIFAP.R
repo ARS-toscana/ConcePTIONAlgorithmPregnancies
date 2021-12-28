f0f2f15f83b05228d994f92cea20affdc54cc708
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-12-28 16:53:36 +0100

    updated procedure codes

--- ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/03_conceptsets/03_conceptsets_BIFAP.R
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/03_conceptsets/03_conceptsets_BIFAP.R
@@ -1,6 +1,6 @@
 # date: TO_ADD
-# datasource: PHARMO
-# DAP: PHARMO
+# datasource: BIFAP
+# DAP: BIFAP
 # author: TO_ADD
 # version: 1.0
 # changelog: 
@@ -8,18 +8,16 @@
 ####### LOAD PROCEDURES for BIFAP
 
 ####### Codes for tests for gestational diabetes ###############
-concept_set_codes_pregnancy_datasource[["gestational_diabetes"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("90.26.7") 
+concept_set_codes_pregnancy_datasource[["gestational_diabetes"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c() 
 
 ####### Codes for fetal nuchal translucency ###############
 
 ####### Codes for amniocentesis ###############
-concept_set_codes_pregnancy_datasource[["amniocentesis"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("75.10.2", "75.10.3")
+concept_set_codes_pregnancy_datasource[["amniocentesis"]][["BIFAP"]][["ICD9"]] <- c("75.1", "75.31", "75.37")
 
 ####### Codes for Chorionic Villus Sampling ###############
-concept_set_codes_pregnancy_datasource[["Chorionic_Villus_Sampling"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("75.10.1")
+concept_set_codes_pregnancy_datasource[["Chorionic_Villus_Sampling"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c()
 
 # ####### Codes for tests for others ###############
-concept_set_codes_pregnancy_datasource[["others"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("75.34.1")
-
-
+concept_set_codes_pregnancy_datasource[["others"]][["BIFAP"]][["ICD9"]] <- c("69.02")
 
