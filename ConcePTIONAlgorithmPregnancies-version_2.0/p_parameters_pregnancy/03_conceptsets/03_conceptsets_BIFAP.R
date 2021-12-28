71edb63a3b1853ea0903bf7f6175289c6cecc404
Claudia Bartolini <clabar@arsanita.toscana.it>
2021-12-28 10:35:34 +0100

    added procedures for BIFAP

---
+++ ConcePTIONAlgorithmPregnancies-version_2.0/p_parameters_pregnancy/03_conceptsets/03_conceptsets_BIFAP.R
@@ -0,0 +1,25 @@
+# date: TO_ADD
+# datasource: PHARMO
+# DAP: PHARMO
+# author: TO_ADD
+# version: 1.0
+# changelog: 
+
+####### LOAD PROCEDURES for PHARMO
+
+####### Codes for tests for gestational diabetes ###############
+concept_set_codes_pregnancy_datasource[["gestational_diabetes"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("90.26.7") 
+
+####### Codes for fetal nuchal translucency ###############
+
+####### Codes for amniocentesis ###############
+concept_set_codes_pregnancy_datasource[["amniocentesis"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("75.10.2", "75.10.3")
+
+####### Codes for Chorionic Villus Sampling ###############
+concept_set_codes_pregnancy_datasource[["Chorionic_Villus_Sampling"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("75.10.1")
+
+# ####### Codes for tests for others ###############
+concept_set_codes_pregnancy_datasource[["others"]][["BIFAP"]][["TO_ADD_coding_system"]] <- c("75.34.1")
+
+
+
