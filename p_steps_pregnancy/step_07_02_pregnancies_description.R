58ca77cf565721c21036e9e3fbc3d1fd0d64289b
giorgio.limoncella <giorgio.limoncella@ars.toscana.it>
2022-07-15 16:17:37 +0200

    updated description

--- p_steps_pregnancy/step_07_02_pregnancies_description.R
+++ p_steps_pregnancy/step_07_02_pregnancies_description.R
@@ -12,6 +12,7 @@ render(paste0(dirmacro,"pregnancies_description_HTML.Rmd"),
        output_dir = dirdescribe,
        output_file = "HTML_pregnancy_description", 
        params=list(D3_pregnancy_reconciled = D3_pregnancy_reconciled_valid,
-                   D3_groups_of_pregnancies_reconciled = D3_groups_of_pregnancies_reconciled))
+                   D3_groups_of_pregnancies_reconciled = D3_groups_of_pregnancies_reconciled,
+                   thisdatasource = thisdatasource))
 
 rm(D3_pregnancy_reconciled, D3_groups_of_pregnancies_reconciled)
