27f2edf9583cd2bd3cd6e50acbc03c8d9f96b304
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2022-10-17 12:37:18 +0200

    commented warning

--- p_parameters_pregnancy/05_check_coding_system.R
+++ p_parameters_pregnancy/05_check_coding_system.R
@@ -11,11 +11,11 @@ allcodes_inMETADATA_nofree<-allcodes_inMETADATA0[tokeep=!(str_detect(allcodes_in
 concept_set_prova<-names(concept_set_domains=="Diagnosis")[1]
 coding_system_in_concept_sets<-names(concept_set_codes_pregnancy[[concept_set_prova]])
 
-if(sum(allcodes_inMETADATA_nofree  %notin% coding_system_in_concept_sets)*1 >0) {
-  warning(c("There is an error in coding system: one or more coding system in your data are not included in the list of coding systems supported by this script.","Please contact the script programmer."))
-  
-  # display the coding systems that are missing
-  print("Missing coding system are:")
-  allcodes_inMETADATA_nofree[allcodes_inMETADATA_nofree  %notin% coding_system_in_concept_sets]
-  
-}
+# if(sum(allcodes_inMETADATA_nofree  %notin% coding_system_in_concept_sets)*1 >0) {
+#   warning(c("There is an error in coding system: one or more coding system in your data are not included in the list of coding systems supported by this script.","Please contact the script programmer."))
+#   
+#   # display the coding systems that are missing
+#   print("Missing coding system are:")
+#   allcodes_inMETADATA_nofree[allcodes_inMETADATA_nofree  %notin% coding_system_in_concept_sets]
+#   
+# }
