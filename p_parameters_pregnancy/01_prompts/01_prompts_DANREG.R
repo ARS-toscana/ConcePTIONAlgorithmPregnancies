70412e4ad3b369079405325e552e8fe040260670
Claudia Bartolini <clabar@arsanita.toscana.it>
2022-10-26 15:45:21 +0200

    added DANREG

---
+++ p_parameters_pregnancy/01_prompts/01_prompts_DANREG.R
@@ -0,0 +1,22 @@
+# date: DANREG
+# datasource: DANREG
+# DAP: DANREG
+# author: DANREG
+# version: 1.0
+# changelog: 
+
+####### LOAD MEANING_OF_SURVEY for DANREG
+
+meaning_of_survey_pregnancy <- vector(mode="list")
+
+meaning_of_survey_pregnancy[["DANREG"]][["livebirth_or_stillbirth"]]<-list("birth_registry")
+meaning_of_survey_pregnancy[["DANREG"]][["ongoing_pregnancy"]]<-list()
+meaning_of_survey_pregnancy[["DANREG"]][["spontaneous_abortion"]]<-list()
+meaning_of_survey_pregnancy[["DANREG"]][["induced_termination"]]<-list()
+meaning_of_survey_pregnancy[["DANREG"]][["other"]]<-list()
+
+
+####### LOAD MEANING_OF_VISIT for DANREG
+
+meaning_of_visit_pregnancy <- vector(mode="list")
+meaning_of_visit_pregnancy[["DANREG"]]<-list()
