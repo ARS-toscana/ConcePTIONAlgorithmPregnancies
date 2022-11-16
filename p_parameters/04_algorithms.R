c01435f6dec104f855ecd419d6e7de059dbddb4a
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2022-11-16 11:29:02 +0100

    added new UOSL meaning

--- p_parameters/04_algorithms.R
+++ p_parameters/04_algorithms.R
@@ -1,8 +1,25 @@
 # we need to create two groups of meanings: one referring to hospitals HOSP (excluding emergency care) and one referring to primary care PC
 
 meanings_of_this_study<-vector(mode="list")
-meanings_of_this_study[["HOSP"]]=c("hospitalisation_primary","hospitalisation_secondary","hospital_diagnosis","hopitalisation_diagnosis_unspecified","episode_primary_diagnosis","episode_secondary_diagnosis","diagnosis_procedure","hospitalisation_associated","hospitalisation_linked","HH","NH")
-meanings_of_this_study[["PC"]]=c("primary_care_event","primary_care_diagnosis","primary_care_events_BIFAP","primary_care_antecedents_BIFAP","primary_care_condicionants_BIFAP")
+meanings_of_this_study[["HOSP"]]=c("hospitalisation_primary",
+                                   "hospitalisation_secondary",
+                                   "hospital_diagnosis",
+                                   "hopitalisation_diagnosis_unspecified",
+                                   "episode_primary_diagnosis",
+                                   "episode_secondary_diagnosis",
+                                   "diagnosis_procedure",
+                                   "hospitalisation_associated",
+                                   "hospitalisation_linked",
+                                   "HH",
+                                   "NH", 
+                                   "hospitalisation_not_overnight_primary")#
+
+meanings_of_this_study[["PC"]]=c("primary_care_event",
+                                 "primary_care_diagnosis",
+                                 "primary_care_events_BIFAP",
+                                 "primary_care_antecedents_BIFAP",
+                                 "primary_care_condicionants_BIFAP", 
+                                 "primary_care_main_diagnosis")#
 
 # create two conditions on the meaning_of_event variable, associated to HOSP and to PC as listed above
 
