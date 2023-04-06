c9a02f011ba1479cda66602297e0cdf2aa551d7c
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2023-04-06 10:43:48 +0200

    commented vocabularies_with_exact_search

--- p_steps_pregnancy/step_01_03_T2.1_create_conceptset_datasets.R
+++ p_steps_pregnancy/step_01_03_T2.1_create_conceptset_datasets.R
@@ -24,8 +24,8 @@ if(this_datasource_has_conceptsets){
                            discard_from_environment = F,
                            dirinput = dirinput,
                            diroutput = dirtemp,
-                           extension = c("csv"),
-                           vocabularies_with_exact_search = "ICD9PROC"#,
+                           extension = c("csv")#,
+                           #vocabularies_with_exact_search = "ICD9PROC"#,
                            #vocabularies_with_dot_wildcard=c("READ")
                            )
   
