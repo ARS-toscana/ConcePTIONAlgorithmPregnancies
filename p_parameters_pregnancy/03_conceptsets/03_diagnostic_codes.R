7cae9eacca3ec1b342cbad3db51e970808d2ddda
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2023-06-08 23:49:59 +0200

    updated codelist

--- p_parameters_pregnancy/03_conceptsets/03_diagnostic_codes.R
+++ p_parameters_pregnancy/03_conceptsets/03_diagnostic_codes.R
@@ -112,8 +112,7 @@ concept_set_codes_pregnancy[["Spontaneousabortion_possible"]] <- list()
 
 
 # loading concepsets from csv 
-#concept_set_codes_pregnancy_data_table <- fread(paste0(thisdir, "/p_parameters_pregnancy/03_conceptsets/20230407_ALL_pregnancy_algorithm_codelist.csv"))
-concept_set_codes_pregnancy_data_table <- fread(paste0(thisdir, "/p_parameters_pregnancy/03_conceptsets/20230427_ALL_pregnancy_algorithm_codelist_fixed_manually.csv"))
+concept_set_codes_pregnancy_data_table <- fread(paste0(thisdir, "/p_parameters_pregnancy/03_conceptsets/20230606_ALL_pregnancy_algorithm_codelist_corrected.csv"))
 
 ###--------------------------------------------------------------------
 # Concept in the FULL codelist ..........Concept used in the algorithm
