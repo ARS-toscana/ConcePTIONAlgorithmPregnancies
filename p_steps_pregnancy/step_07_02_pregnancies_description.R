0502e8730a085c63f8534e536b1dfb95366a5d18
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2023-08-19 12:06:39 +0200

    added description for 2015-2019

--- p_steps_pregnancy/step_07_02_pregnancies_description.R
+++ p_steps_pregnancy/step_07_02_pregnancies_description.R
@@ -15,4 +15,20 @@ render(paste0(dirmacro,"pregnancies_description_HTML.Rmd"),
                    D3_groups_of_pregnancies_reconciled = D3_groups_of_pregnancies_reconciled,
                    thisdatasource = thisdatasource))
 
+
+# 2015-2019
+D3_pregnancy_reconciled_valid <- D3_pregnancy_reconciled_valid[pregnancy_start_date >= 2015 & pregnancy_start_date <= 2019]
+D3_groups_of_pregnancies_reconciled <- D3_groups_of_pregnancies_reconciled[pregnancy_start_date >= 2015 & pregnancy_start_date <= 2019]
+
+if(D3_pregnancy_reconciled_valid[,.N] > 0 & D3_groups_of_pregnancies_reconciled[,.N] > 0){
+  render(paste0(dirmacro,"pregnancies_description_HTML.Rmd"),           
+         output_dir = dirdescribe,
+         output_file = "HTML_pregnancy_description_2015_2019", 
+         params=list(D3_pregnancy_reconciled = D3_pregnancy_reconciled_valid,
+                     D3_groups_of_pregnancies_reconciled = D3_groups_of_pregnancies_reconciled,
+                     thisdatasource = thisdatasource))
+}
+
+
+
 rm(D3_pregnancy_reconciled, D3_groups_of_pregnancies_reconciled)
