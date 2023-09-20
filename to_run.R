2204b603c3fd2beb62298c2517353cb810ed1252
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2023-09-20 11:18:04 +0200

    updated changelog

--- to_run.R
+++ to_run.R
@@ -23,6 +23,7 @@
 # Changelog 5.0.5, 18 September 2023
 # - fixed bug for missing itemset "end_date"
 # - fixed bug g_verification sampling
+# - added parameters for RDRU_FISABIO
 #------------------------------------------------------------------------------------------
 
 
