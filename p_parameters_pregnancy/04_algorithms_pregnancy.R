1c6843a1946f6390a86a0735b4102922229e03ea
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2026-01-30 09:48:45 +0100

    added fix for ICD10DA

--- p_parameters_pregnancy/04_algorithms_pregnancy.R
+++ p_parameters_pregnancy/04_algorithms_pregnancy.R
@@ -168,6 +168,17 @@ if(this_datasource_has_conceptsets){
 }
 
 
+
+if(this_datasource_has_conceptsets){
+  for (conceptset in concept_set_pregnancy){
+    concept_set_codes_pregnancy[[conceptset]][["ICD10DA"]] <- paste0('D', concept_set_codes_pregnancy[[conceptset]][["ICD10"]])
+    
+  }
+}
+
+
+
+
 # fix for ICPC2P
 # for (conceptset in concept_set_pregnancy){
 #   if (length(concept_set_codes_pregnancy[[conceptset]][["ICPC2P"]]) >0 ){
